fb4e16d28deec8c3d149225c76574e3816c6f660
Guillermo Rivas <gmo.rivas@tec.mx>
2025-04-08 18:45:34 -0600

    clase de abril 8

--- App.tsx
+++ App.tsx
@@ -1,6 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// AMBIENTES (ENVIRONMENTS)
+// en el ambito laboral se van a encontrar con diferentes stages en producción / deployment
+
+// normalmente hay al menos 2 
+// desarrollo, producción
+// (pruebas, staging, etc)
+
+// VARIABLES DE AMBIENTE
+// ¿por qué usarlas?
+// 1. no quiero subir información privilegiada como API keys
+// 2. lo más probable es que tengas diferentes datos dependiendo el contexto
+// -- api keys
+// -- URLs 
+// -- cualquier dato de credenciales / validación
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 export default function App() {
   return (
     <View style={styles.container}>
